ab0e6713bc165f2cf5071554b551ea88cfb38313
Andrey Yakushin <a.yakushin@adguard.com>
2026-03-11 09:48:37 +0000

    Pull request 178: Fix sending deferred headers after StreamBlocked
    Squashed commit of the following:
    
    commit 7b93fde43d86b7398f6e684dd33c853483f7415a
    Author: boommy <a.yakushin>
    Date:   Tue Mar 3 17:13:53 2026 +0400
    
        Make flush do nothing by default
    
    commit 791d1906a53fe38747279fcf2e3788a71493da43
    Author: boommy <a.yakushin>
    Date:   Tue Mar 3 17:13:44 2026 +0400
    
        Do not send deferred headers in write method
    
    commit 03075b729d787a13311b4a8006fa70c41014cf41
    Author: boommy <a.yakushin>
    Date:   Tue Mar 3 17:10:54 2026 +0400
    
        Flush at the start of data exchange
    
    commit 13a420a4a73b1298a99e01cbfa6ea3e641495ce9
    Author: boommy <a.yakushin>
    Date:   Tue Mar 3 17:05:41 2026 +0400
    
        Consume deferred headers in flush method and utilize WaitingWritable event for it
    
    commit 08ccb5b21613004e738746e906c65b6b6d1d2c76
    Author: boommy <a.yakushin>
    Date:   Tue Mar 3 17:04:15 2026 +0400
    
        Remove consuming deferred headers from wait_writable
    
    commit fb1305d2646ce2c1c4f42492ce1dbeef62046a70
    Author: boommy <a.yakushin>
    Date:   Tue Mar 3 17:02:12 2026 +0400
    
        Extend ulimit for bench remote container

--- lib/src/http3_codec.rs
+++ lib/src/http3_codec.rs
@@ -431,22 +431,6 @@ impl StreamSink {
         Ok(())
     }
 
-    async fn consume_pending_response(&mut self) -> io::Result<()> {
-        while self.pending_response.is_some() {
-            self.try_send_pending_response()?;
-            if self.pending_response.is_some() {
-                self.codec_tx
-                    .send(StreamMessage::WaitingWritable(self.stream_id))
-                    .map_err(|_| io::Error::from(ErrorKind::UnexpectedEof))?;
-                match self.writable_event_rx.recv().await {
-                    None => return Err(io::Error::from(ErrorKind::UnexpectedEof)),
-                    Some(_) => continue,
-                }
-            }
-        }
-        Ok(())
-    }
-
     async fn wait_body_capacity(&mut self) -> io::Result<()> {
         loop {
             match self.socket.stream_capacity(self.stream_id) {
@@ -508,10 +492,9 @@ impl pipe::Sink for StreamSink {
     }
 
     fn write(&mut self, data: Bytes) -> io::Result<Bytes> {
-        self.try_send_pending_response()?;
         if self.pending_response.is_some() {
             log_id!(
-                debug,
+                error,
                 self.id,
                 "Body write deferred: response headers not yet sent"
             );
@@ -542,8 +525,20 @@ impl pipe::Sink for StreamSink {
     }
 
     async fn wait_writable(&mut self) -> io::Result<()> {
-        self.consume_pending_response().await?;
         self.wait_body_capacity().await
+    }
+
+    async fn flush(&mut self) -> io::Result<()> {
+        while self.pending_response.is_some() {
+            self.codec_tx
+                .send(StreamMessage::WaitingWritable(self.stream_id))
+                .map_err(|_| io::Error::from(ErrorKind::UnexpectedEof))?;
+            match self.writable_event_rx.recv().await {
+                None => return Err(io::Error::from(ErrorKind::UnexpectedEof)),
+                Some(_) => self.try_send_pending_response()?,
+            }
+        }
+        Ok(())
     }
 }
 
